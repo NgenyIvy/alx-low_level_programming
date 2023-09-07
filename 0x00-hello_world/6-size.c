21c20094d8372f46df1b3bb10d4a049ac0990b0b
NgenyIvy <cherotichivy2000@gmail.com>
2023-09-07 15:24:18 -0700

    My second commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char is :%lu byte(s)\n", (unsigned long) sizeof(c));
+	printf("Size of a int is :%lu byte(s)\n", (unsigned long) sizeof(i));
+	printf("Size of a long int is :%lu byte(s)\n", (unsigned long) sizeof(li));
+	printf("Size of a long long int :%lu byte(s)\n", (unsigned long) sizeof(lli));
+	printf("Size of a float is :%lu byte(s)\n", (unsigned long) sizeof(f));
+	return (0);
+}
