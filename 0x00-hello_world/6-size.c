62c2b60200615d6701ac000a0b49bda65c9d8cc4
NgenyIvy <cherotichivy2000@gmail.com>
2023-09-07 16:16:15 -0700

    Fifth commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@
 	long long int lli;
 	float f;
 
-	printf("Size of a char is :%lu byte(s)\n", (unsigned long) sizeof(c));
+printf("Size of a char is :%lu byte(s)\n", (unsigned long) sizeof(c));
 	printf("Size of a int is :%lu byte(s)\n", (unsigned long) sizeof(i));
-	printf("Size of a long int is :%lu byte(s)\n", (unsigned long) sizeof(li));
-	printf("Size of a long long int :%lu byte(s)\n", (unsigned long) sizeof(lli));
-	printf("Size of a float is :%lu byte(s)\n", (unsigned long) sizeof(f));
+printf("Size of a long int is :%lu byte(s)\n", (unsigned long) sizeof(li));
+printf("Size of a long long int :%lu byte(s)\n", (unsigned long) sizeof(lli));
+printf("Size of a float is :%lu byte(s)\n", (unsigned long) sizeof(f));
 	return (0);
 }
