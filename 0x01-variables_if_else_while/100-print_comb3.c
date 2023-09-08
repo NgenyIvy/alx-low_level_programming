334a5364d5c9d9d6e04ee2d957f564be19cd1e86
NgenyIvy <cherotichivy2000@gmail.com>
2023-09-08 16:25:57 -0700

    Modifications on 9, 101 and 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,17 +9,16 @@ int main(void)
 	int j;
 
 	for (i = 0; i <= 9; i++)
-		for (j = 0; j <= 9; j++)
-			
-			if ((i != j) && (i < j))
-					{
-					if ((i == 0 || 1) && (j != 0 || 1))
-					putchar(i +48);
-					putchar(j +48);
-					if ((i != 8) && (j != 9))
-					putchar(44);
-					putchar(32);
-					}
+	for (j = 0; j <= 9; j++)
+		if ((i != j) && (i < j))
+		{
+			if ((i == 0 || 1) && (j != 0 || 1))
+			putchar(i + 48);
+			putchar(j + 48);
+			if ((i != 8) && (j != 9))
+			putchar(44);
+			putchar(32);
+		}
 putchar('\n');
 return (0);
 }
