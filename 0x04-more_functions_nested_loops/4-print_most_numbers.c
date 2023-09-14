31ec53a69882b548befd943703be6ff370401714
NgenyIvy <cherotichivy2000@gmail.com>
2023-09-14 05:51:41 -0700

    4-print_most_numbers.c, 4-main.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *print_most_numbers - Prints most numbers
+  *@c: Character to be tested
+  *Return - Always 0
+  */
+void print_most_numbers(void)
+{
+	char c;
+	for (c = 48; c <= 57; c++)
+	{
+		if (!(c == 50 || c == 52))
+			_putchar(c);
+	}
+	_putchar('\n');
+}
