978d82a04ae4184875f36c903f31e66206c77187
NgenyIvy <cherotichivy2000@gmail.com>
2023-09-14 04:50:23 -0700

    2-main.c, 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * mul - multiply twi integers
+  *@a: -one of the integers
+  *@b: - another integer
+  *Return: Returns the product of the to numbers
+  */
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+	return (prod);
+}
