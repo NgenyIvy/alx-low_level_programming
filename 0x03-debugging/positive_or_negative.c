26f12ff56a57efbb8a1ef1af2042cc71963b132c
NgenyIvy <cherotichivy2000@gmail.com>
2023-09-15 05:15:29 -0700

    3-print_remaining_days.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,17 +1,20 @@
 #include "main.h"
-
+/**
+  *positive_or_negative - Checks for positive or negative
+  *@i:Integer
+  */
 void positive_or_negative(int i)
 {
 	if (i > 0)
-        {
-                printf("%d is positive\n", i);
-        }
-        else if (i == 0)
 	{
-                printf("%d is zero\n", i);
+		printf("%d is positive\n", i);
+	}
+	else if (i ==)
+	{
+		printf("%d is zero\n", i);
 	}
 	else
-        {
-                printf("%d is negative\n", i);
-        }
+	{
+		printf("%d is negative\n", i);
+	}
 }
