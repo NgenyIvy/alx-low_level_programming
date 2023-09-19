fc91ae1198a12662ce504f084558031931994ecf
NgenyIvy <cherotichivy2000@gmail.com>
2023-09-18 19:57:32 -0700

    7-puts_half.c modification

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+void puts_half(char *str) {
+    int length = strlen(str);
+    
+    int start_index = (length - 1) / 2;
+    int i;
+    for (i = start_index; i < length; i++) {
+        putchar(str[i]);
+    }
+    
+    putchar('\n');
+}
