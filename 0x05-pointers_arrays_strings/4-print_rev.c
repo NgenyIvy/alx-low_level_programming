3c2aade7b0b8486380de7d0b2750470f02ba821e
NgenyIvy <cherotichivy2000@gmail.com>
2023-09-19 15:43:43 -0700

    4-print_rev.c, 4-main.c modification

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,21 @@
 #include"main.h"
 #include <stdio.h>
 #include <string.h>
+/**
+  *print_rev - imprime en reversa
+  *@s: string
+  *return: 0
+  */
+void print_rev(char *s)
+{
+	if (s != NULL)
+	{
+		int length = strlen(s);
+		int i;
 
-void print_rev(char *s) {
-    if (s != NULL) {
-        int length = strlen(s);
-	int i;
-	for (i = length - 1; i >= 0; i--) {
-            printf("%c", s[i]);
-        }
-        printf("\n");
-    }
+		for (i = length - 1; i >= 0; i--)
+		printf("%c", s[i]);
+	printf("\n");
+	}
 }
 
