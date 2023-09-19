f2520367be3a67cbbfd362c5fdceb6df46eaf468
NgenyIvy <cherotichivy2000@gmail.com>
2023-09-19 16:29:02 -0700

    7-puts_half.c, 7-main.c modification

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,15 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+/**
+  *puts_half - Returns half of the srting
+  *@str: String
+  *Return:length
+  */
+void puts_half(char *str)
+{
+	int length = strlen(str);
+	int start_index = (length - 1) / 2;
+	int i;
 
-void puts_half(char *str) {
-    int length = strlen(str);
-    
-    int start_index = (length - 1) / 2;
-    int i;
-    for (i = start_index; i < length; i++) {
-        putchar(str[i]);
-    }
-    
-    putchar('\n');
+	for (i = start_index; i < length; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
 }
